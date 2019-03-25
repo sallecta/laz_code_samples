940ca5a092b29dccbbab373d3179973f9b4288fe
sallecta <sallecta@inbox.ru>
2019-03-26 00:56:56 +0300

    update gitpush script

--- gitpush.sh
+++ gitpush.sh
@@ -1,27 +1,43 @@
 #!/bin/bash
 
-#version 1.2
-#git add *
-#git add **/.gitignore
-#git add .gitignore
-git add -u 
+#version 1.6
+git add *
+git add **/.gitignore
+git add .gitignore
+git add -u
+echo 'Check status:' 
 git status
-echo 'Check status:'
-echo '- write commit message or just press Enter to upload;'
-echo '- type Ctrl+C to cancel.'
-read commmitMessage
 
-if [ "$commmitMessage" = "" ]
+if [ "$1" = "withCommit" ]
+then
+    commmitMessageArg="update gitpush script"
+    echo "- press Enter to accept commit message \"$commmitMessageArg\" and upload (push);"
+    echo '- type new non empty commit message and press Enter to upload (push);'
+else
+    echo "- press Enter to upload (push) with commit message \"...\""
+    echo '- type commit message and press Enter to upload (push);'
+fi
+
+echo '- type Ctrl+C to cancel.'
+read commmitMessageUsr
+
+if [ "$commmitMessageUsr" = "" ]  && [ "$commmitMessageArg" = "" ]
 then
     commmitMessage='...'
 fi
+if [ ! "$commmitMessageArg" = "" ]  && [ "$commmitMessageUsr" = "" ]
+then
+    commmitMessage=$commmitMessageArg
+fi
+if [ ! "$commmitMessageUsr" = "" ]
+then
+    commmitMessage=$commmitMessageUsr
+fi
+
 echo commit message is \"$commmitMessage\"
+
 git commit --message="$commmitMessage"
 
-if [ "$commmitMessage" = "" ]
-then
-    commmitMessage='...'
-fi
 git push
 
 retVal=$?
